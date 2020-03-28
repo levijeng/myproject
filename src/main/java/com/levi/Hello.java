32120d8ee84eb90aa4fc2fe0c15fc973013e2517
Levi Jeng <oohlevi@gmail.com>
2020-03-28 22:30:30 +0800

    Java and Kotlin variables, data type

--- src/main/java/com/levi/Hello.java
+++ src/main/java/com/levi/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //        System.out.println("Hello World");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = c;
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
     }
 }
--- src/main/kotlin/com/levi/Hello.kt
+++ src/main/kotlin/com/levi/Hello.kt
@@ -5,6 +5,9 @@ fun main() {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age = 19
+    var weight = 12.8
+    var name : String
 }
 
 class Human {
