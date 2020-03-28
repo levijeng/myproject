0f17653046aa6853414f3adfe59ec1a2243b45e6
Levi Jeng <oohlevi@gmail.com>
2020-03-28 18:22:00 +0800

    Initial commit

---
+++ src/main/java/com/levi/Person.java
@@ -0,0 +1,7 @@
+package com.levi;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/levi/Person2.java
@@ -0,0 +1,7 @@
+package com.levi;
+
+public class Person2 {
+    public void hello() {
+        System.out.println("Hello Person2");
+    }
+}
